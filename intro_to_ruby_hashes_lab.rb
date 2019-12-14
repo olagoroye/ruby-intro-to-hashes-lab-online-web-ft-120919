3fb667e636c4f8a25b1c43e02466dbf912d7ddfa
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-14 02:56:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,7 @@ monopoly = {:railroads => {
   :names => {:reading_railroad => {},
     :pennsylvania_railroad => {},
     :b_and_o_railroad => {},
+    :shortline => {}
   },
   :rent_in_dollars => {
     :one_piece_owned => 25,
